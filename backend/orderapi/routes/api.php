79b06708ae7f7e42d6e89df0de9591cb3231fd10
Jeison Velez <Velezjeison97@gmail.com>
2024-03-08 09:14:43 -0500

    controllers part 2
    Se crearon los controladores para activity y order

--- backend/orderapi/routes/api.php
+++ backend/orderapi/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\CausalController;
 use App\Http\Controllers\ObservationController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\TechnicianController;
 use App\Http\Controllers\TypeActivityController;
+use App\Models\Activity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +31,7 @@ Route::apiResource('observation', ObservationController::class);
 
 Route::apiResource('type_activity', TypeActivityController::class);
 Route::apiResource('technician', TechnicianController::class);
-
+Route::apiResource('activity', ActivityController::class);
+Route::apiResource('order', OrderController::class);
+Route::get('order/add_activity/{order}/{activity}',[OrderController::class, 'add_activity'])->name('order.add_activity');
+Route::get('order/remove_activity/{order}/{activity}',[OrderController::class, 'remove_activity'])->name('order.remove_activity');
